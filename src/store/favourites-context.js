e2894a3fc72284a0032f2b3309c72624161e26cb
ScorchingShade <ankushors789@gmail.com>
2021-09-15 17:45:43 +0530

    added store fucntions

--- src/store/favourites-context.js
+++ src/store/favourites-context.js
@@ -1,21 +1,38 @@
-import {createContext, useState} from 'react';
+import { createContext, useState } from "react";
+
+const FavouritesContext = createContext({
+  favourites: [],
+  totalFavourites: 0,
+});
+
+function FavouritesContextProvider(props) {
+  const [userFavourites, setUserFavourites] = useState(initialState);
+
+  const addFavouriteHandler = (favouriteMeetup) => {
+    setUserFavourites((prevUserFavourites) => {
+      return prevUserFavourites.concat(favouriteMeetup);
+    });
+  };
 
 
-const FavouritesContext=createContext({
-    favourites:[],
-    totalFavourites:0
-})
+  const removeFavouriteHandler=(meetupId)=>{
+    setUserFavourites((prevUserFavourites) => {
+        return prevUserFavourites.filter(meetup=>meetup.id!==meetupId);
+      });
+  }
 
-function FavouritesContextProvider(props){
+  const itemIsFavouriteHandler = (meetupId)=>{
+      return userFavourites.some(meetup=>meetup.id===meetupid);
+  }
 
-    const [userFavourites, setUserFavourites] = useState(initialState)
+  const context = {
+    favourites: userFavourites,
+    totalFavourites: userFavourites.length,
+  };
 
-    const context={
-        favourites:userFavourites,
-        totalFavourites:userFavourites.length
-    };
-
-    return <FavouritesContext.Provider value={context} >;
-        {props.children}
+  return (
+    <FavouritesContext.Provider value={context}>
+      ;{props.children}
     </FavouritesContext.Provider>
+  );
 }
