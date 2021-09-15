7a6c328ed12ee395a37353f34719ff8cba5dd4a7
ScorchingShade <ankushors789@gmail.com>
2021-09-15 16:04:35 +0530

    added firebase support form form fill

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import NewMeetupForm from '../components/meetups/NewMeetupForm'
+import React from "react";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import axios from "axios";
 
 function NewMeetup() {
-    return (
-        <section>
-            <h1>Add New Meetup</h1>
-            <NewMeetupForm/>
-        </section>
-    )
+  const addMeetupHandler = (meetupData) => {
+    axios
+      .post(
+        "https://react-refresh-33212-default-rtdb.firebaseio.com/meetups.json",
+        JSON.stringify(meetupData)
+      )
+      .then((message) => console.log(message))
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
-export default NewMeetup
+export default NewMeetup;
