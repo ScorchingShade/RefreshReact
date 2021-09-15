5f38454440d4b6f3393ecd8533b155213cfbe4ce
ScorchingShade <ankushors789@gmail.com>
2021-09-15 16:08:58 +0530

    added form post feedback and useHistory

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,16 +1,20 @@
 import React from "react";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 import axios from "axios";
+import { useHistory } from "react-router";
+
 
 function NewMeetup() {
+    const history=useHistory();
+
   const addMeetupHandler = (meetupData) => {
     axios
       .post(
         "https://react-refresh-33212-default-rtdb.firebaseio.com/meetups.json",
         JSON.stringify(meetupData)
       )
-      .then((message) => console.log(message))
-      .catch((error) => console.error(error));
+      .then((message) => history.replace('/'))
+      .catch((error) => alert("There seems to be some issue accessing the server! Please try again later"));
   };
 
   return (
