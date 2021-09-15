436928fbebe868cec6a9e184c1e571681706ed18
ScorchingShade <ankushors789@gmail.com>
2021-09-15 18:09:56 +0530

    added badge and used context in favourites to load

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import FavouritesContext from '../../store/favourites-context';
 import classes from './MainNavigation.module.css'
 
 function MainNavigation() {
+
+    const favouritesCtx=useContext(FavouritesContext);
     
     return (
         <header className={classes.header}>
@@ -16,7 +19,9 @@ function MainNavigation() {
                         <Link to="/new-meetup">new meetup</Link>
                     </li>
                     <li>
-                        <Link to="/favourites">favourites</Link>
+                        <Link to="/favourites">favourites
+                            <span className={classes.badge}>{favouritesCtx.totalFavourites}</span>
+                        </Link>
                     </li>
                 </ul>
             </nav>
--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import MeetupList from '../components/meetups/MeetupList';
+import FavouritesContext from '../store/favourites-context'
 
 function Favourites() {
+    const favouritesCtx=useContext(FavouritesContext);
+
+
+    let content;
+
+
+    if(favouritesCtx.totalFavourites===0){
+        content=<p>You got no favourites yet. Start adding some now to view them!</p>
+    }
+    else{
+        content=<MeetupList meetups={favouritesCtx.favourites}/>
+    }
+
+
+
     return (
-        <div>
-            Favourites
-        </div>
+        <section>
+            <h1>My Favourites</h1>
+            {content}
+
+        </section>
     )
 }
 
