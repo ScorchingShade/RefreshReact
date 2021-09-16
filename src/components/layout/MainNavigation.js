069a80dbd4a721701da122fa43ef0f364a46781b
ScorchingShade <ankushors789@gmail.com>
2021-09-17 03:53:32 +0530

    sending new deployment code

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -9,7 +9,7 @@ function MainNavigation() {
     
     return (
         <header className={classes.header}>
-            <div className={classes.logo}>React Meetups</div>
+            <div className={classes.logo}> <Link to="/">React Meetups</Link></div>
             <nav>
                 <ul>
                     <li>
